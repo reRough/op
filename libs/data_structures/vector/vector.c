b6ed933f618256507f4d28187682cd29787dba4d
reRough <dijck.xf@gmail.com>
2022-02-15 17:38:01 +0300

    first steps

---
+++ libs/data_structures/vector/vector.c
@@ -0,0 +1,5 @@
+//
+// Created by Admin on 15.02.2022.
+//
+
+#include "vector.h"
---
+++ libs/data_structures/vector/vector.h
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 15.02.2022.
+//
+
+#ifndef LABS_LIBS_VECTOR_H
+#define LABS_LIBS_VECTOR_H
+
+#endif //LABS_LIBS_VECTOR_H
