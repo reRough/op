c8f15a3d70981fc628bef05d4fc1c92d50c31bcf
reRough <dijck.xf@gmail.com>
2022-03-30 01:10:35 +0300

    fourth

--- libs/data_structures/matrix/tasks.c
+++ libs/data_structures/matrix/tasks.c
@@ -42,3 +42,20 @@ void sortRowsByMaxElement(matrix m) {
     insertionSortRowsMatrixByRowCriteria(m, getMax);
 }
 
+matrix mulMatrices(matrix m1, matrix m2) {
+    assert(m1.nCols == m2.nRows);
+    matrix m3 = createMatrix(m1.nRows, m2.nCols);
+    for (int i = 0; i < m3.nRows; i++)
+        for (int j = 0; j < m3.nCols; j++) {
+            m3.values[i][j] = 0;
+            for (size_t k = 0; k < m1.nCols; k++)
+                m3.values[i][j] += m1.values[i][k] * m2.values[k][j];
+        }
+    return m3;
+}
+
+void getSquareOfMatrixIfSymmetric(matrix *m) {
+    if (isSquareMatrix(*m) && isSymmetricMatrix(*m)) {
+        *m = mulMatrices(*m, *m);
+    }
+}
