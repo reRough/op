9216d9868d97f7840e1bc4c0c2b917dbd91d33dd
reRough <dijck.xf@gmail.com>
2022-04-02 14:17:39 +0300

    first functions

--- libs/string/string_.c
+++ libs/string/string_.c
@@ -3,3 +3,18 @@
 //
 
 #include "string_.h"
+
+size_t strlen(const char *begin) {
+    const char *end = begin;
+    while (*end != '\0')
+        end++;
+
+    return end - begin;
+}
+
+char *find(char *begin, char *end, int ch) {
+    while (begin != end && *begin != ch)
+        begin++;
+
+    return begin;
+}
--- libs/string/string_.h
+++ libs/string/string_.h
@@ -5,4 +5,15 @@
 #ifndef LABS_LIBS_STRING__H
 #define LABS_LIBS_STRING__H
 
+#include <stdlib.h>
+
+//возвращает количество символов в строке (не считая ноль-символ)
+size_t strlen(const char *begin);
+
+//возвращает указатель на первый элемент с кодом ch,
+// расположенным на ленте памяти между адресами begin и end не включая end
+char *find(char *begin, char *end, int ch);
+
+
+
 #endif //LABS_LIBS_STRING__H
